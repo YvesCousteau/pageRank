79d867eac717093aecacbec54d064e598bffa2b4
Hugo <YvesCousteau>
2022-02-16 12:07:19 +0100

    setup

---
+++ script/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Makefile
+../make
+# Run pageRank
+../pageRank
+# Plot courbe
+FILES="clock_*"
+cd ../data
+for f in $(ls clock_*)
+do
+  gnuplot -c ../script/gnuplot.gp $f > ../plot/plot_${f%.*}.png
+  eog ../plot/plot_${f%.*}.png
+done
+cd ../script
